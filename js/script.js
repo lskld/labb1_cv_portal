d654f8cd56640cb91e650eb896f85e3f650b4f89
lskld <theo.lillieskold@gmail.com>
2026-02-14 10:05:57 +0100

    Added beginning of a modal + simple js to project page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+var projectModal1 = document.getElementById("card1_modal");
+var projectModal2 = document.getElementById("card2_modal");
+var projectModal3 = document.getElementById("card3_modal");
+
+var projectBtn1 = document.getElementById("card1_btn")
+var projectBtn2 = document.getElementById("card2_btn")
+var projectBtn3 = document.getElementById("card3_btn")
+
+projectBtn1.onclick = function () {
+    projectModal1.style.display = "block";
+}
+
